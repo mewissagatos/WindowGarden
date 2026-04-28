080f29b9dab6aaffb9418ea34c74de9e21024820
CoDy <pedrofelipe8553@gmail.com>
2026-04-28 11:33:47 -0300

    Add files via upload
    Backend do server e o html da front end inicial
    28/04/2026
    11:33

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Configuração da conexão com o MySQL//laragon
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',      
+    password: '12345678',      // <--- senha padrão do host do laragon, altere se necessário
+    database: 'meu_jardim' 
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Erro ao conectar ao MySQL:', err);
+        return;
+    }
+    console.log('Conectado ao banco de dados!');
+});
+
+// Rota para buscar todas as plantas
+app.get('/plantas', (req, res) => {
+    const sql = "SELECT * FROM plantas_cultivo";
+    db.query(sql, (err, data) => {
+        if (err) return res.status(500).json(err);
+        return res.json(data);
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000");
+});
